7dfb02ee342e35bb1e11d2434a10b1e7b5db9346
kien8995 <trandongkien.sine@gmail.com>
2024-02-05 03:29:03 +0700

    update

---
+++ scripts/block-ads-keywords.js
@@ -0,0 +1,35 @@
+function isAdScriptUrl(url) {
+    // Define a list of keywords that are commonly found in ad script URLs
+    const adKeywords = [
+        "ad",
+        "ads",
+        "advert",
+        "advertisement",
+        "advertising",
+        "doubleclick",
+        "googleads",
+        "adserver",
+        "adtech",
+    ];
+
+    // Parse the URL to inspect its components
+    const parsedUrl = new URL(url);
+
+    // Check the hostname and pathname for any of the keywords
+    const hostnameContainsAdKeyword = adKeywords.some((keyword) =>
+        parsedUrl.hostname.toLowerCase().includes(keyword)
+    );
+    const pathnameContainsAdKeyword = adKeywords.some((keyword) =>
+        parsedUrl.pathname.toLowerCase().includes(keyword)
+    );
+
+    // Return true if any of the components contain ad-related keywords
+    return hostnameContainsAdKeyword || pathnameContainsAdKeyword;
+}
+
+const url = $request.url;
+if (isAdScriptUrl(url)) {
+    $done();
+} else {
+    $done({});
+}

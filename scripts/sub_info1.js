738c34e74e27bf151a26fcada7abb8e9efdb5638
kien8995 <trandongkien.sine@gmail.com>
2024-02-09 18:08:08 +0700

    update

--- scripts/sub_info1.js
+++ scripts/sub_info1.js
@@ -11,7 +11,7 @@ let params = getParams($argument);
 
     $done({
         title: "Surge Pro",
-        content: `${params.url}`,
+        content: `${$argument}`,
         icon: params.icon,
         "icon-color": params.color,
     });

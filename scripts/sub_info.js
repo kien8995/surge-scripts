f4d0154455411d3779e98326e9f888976e563ffc
kien8995 <trandongkien.sine@gmail.com>
2024-02-09 15:18:23 +0700

    update

--- scripts/sub_info.js
+++ scripts/sub_info.js
@@ -1,49 +1,58 @@
-let args = getArgs();
+// let args = getArgs();
 
 !(async () => {
-    // let info = await getDataInfo(args.url);
-    let info = true;
-    if (!info) {
-        $done({
-            title: "Title",
-            content: "No content",
-        });
-    }
     $done({
-        title: `${args.title} artsrt`,
-        content: "rstodienrstedn",
-        icon: args.icon || "airplane.circle",
-        "icon-color": args.color || "#007aff",
+        title: "Surge Pro",
+        content: `Startup duration:`,
+        icon: params.icon,
+        "icon-color": params.color,
     });
-
-    // let resetDayLeft = getRmainingDays(parseInt(args["reset_day"]));
-
-    // let used = info.download + info.upload;
-    // let total = info.total;
-    // let expire = args.expire || info.expire;
-    // let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}`];
-
-    // if (resetDayLeft) {
-    //     content.push(`重置：剩余${resetDayLeft}天`);
-    // }
-    // if (expire && expire !== "false") {
-    //     if (/^[\d.]+$/.test(expire)) expire *= 1000;
-    //     content.push(`到期：${formatTime(expire)}`);
-    // }
-
-    // let now = new Date();
-    // let hour = now.getHours();
-    // let minutes = now.getMinutes();
-    // hour = hour > 9 ? hour : "0" + hour;
-    // minutes = minutes > 9 ? minutes : "0" + minutes;
-
-    // $done({
-    //     title: `${args.title} | ${hour}:${minutes}`,
-    //     content: content.join("\n"),
-    //     icon: args.icon || "airplane.circle",
-    //     "icon-color": args.color || "#007aff",
-    // });
 })();
+
+// !(async () => {
+//     // let info = await getDataInfo(args.url);
+//     let info = true;
+//     if (!info) {
+//         $done({
+//             title: "Title",
+//             content: "No content",
+//         });
+//     }
+//     $done({
+//         title: `${args.title} artsrt`,
+//         content: "rstodienrstedn",
+//         icon: args.icon || "airplane.circle",
+//         "icon-color": args.color || "#007aff",
+//     });
+
+// let resetDayLeft = getRmainingDays(parseInt(args["reset_day"]));
+
+// let used = info.download + info.upload;
+// let total = info.total;
+// let expire = args.expire || info.expire;
+// let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}`];
+
+// if (resetDayLeft) {
+//     content.push(`重置：剩余${resetDayLeft}天`);
+// }
+// if (expire && expire !== "false") {
+//     if (/^[\d.]+$/.test(expire)) expire *= 1000;
+//     content.push(`到期：${formatTime(expire)}`);
+// }
+
+// let now = new Date();
+// let hour = now.getHours();
+// let minutes = now.getMinutes();
+// hour = hour > 9 ? hour : "0" + hour;
+// minutes = minutes > 9 ? minutes : "0" + minutes;
+
+// $done({
+//     title: `${args.title} | ${hour}:${minutes}`,
+//     content: content.join("\n"),
+//     icon: args.icon || "airplane.circle",
+//     "icon-color": args.color || "#007aff",
+// });
+// })();
 
 function getArgs() {
     return Object.fromEntries(

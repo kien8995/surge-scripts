192a528d0994bc876d6c04e2d06bb4d3dc2810c0
kien8995 <trandongkien.sine@gmail.com>
2024-02-13 01:52:48 +0700

    update

--- scripts/get-speed-test-config.js
+++ scripts/get-speed-test-config.js
@@ -1,3 +1,3 @@
-// let url = "https://www.speedtest.net/api/ios-config.php";
-let url = "https://www.speedtest.net/api/js/servers";
+let url = "https://www.speedtest.net/api/ios-config.php";
+// let url = "https://www.speedtest.net/api/js/servers";
 $done($httpClient.get(url));

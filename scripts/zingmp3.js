af89af0daf7d47e6a856eac0bcac5970f8dbb0d5
kien8995 <trandongkien.sine@gmail.com>
2024-02-14 22:06:15 +0700

    update

--- scripts/zingmp3.js
+++ scripts/zingmp3.js
@@ -1,5 +1,2 @@
-body = $response.body.replace(
-    /"lastVipExpireTime":+\d{10}/g,
-    '"vip":{"expireTime":1892800999,"startTime":1572527803,"subscription":{"status":1,"expireTime": 1892800999,"platform":2},"vipType":1}'
-);
+body = $response.body.replace('"hasVipTrial": false', '"hasVipTrial": true');
 $done({ body });

62a1b9a5bac11d92da312d3bd2abab96d70b2060
kien8995 <trandongkien.sine@gmail.com>
2024-02-20 05:09:13 +0700

    update

--- scripts/net_speed.js
+++ scripts/net_speed.js
@@ -1,16 +1,15 @@
 /*
-作者：@xream @keywos @wuhu_zzz @ TEXAS @整点猫咪 技术指导：整点薯条 
-整点花里胡哨
-各种花里胡哨参数，通过argument传入，用=连接key及相应value，用&链接各种key，可以任意选择想填入的参数
-title：标题
-mb参数：每次测试消耗的流量，经测试最大可4MB参数：&mb=4，默认3MB
-iconfast、iconmid、iconslow 分别对应测速快中慢时的图标
-colorlow、colormid、colorhigh 分别对应延迟低中高时的图标颜色
-配置实例：title=花里胡哨才是生产力&iconfast=bird&iconmid=hare&iconslow=tortoise&colorlow=#06D6A0&colormid=#FFD166&colorhigh=#EF476F
+All the bells and whistles
+All kinds of fancy parameters are passed in through argument. Use = to connect the key and the corresponding value. Use & to connect various keys. You can choose the parameters you want to fill in.
+title：title
+mb parameter: the traffic consumed in each test, the maximum limit is 4MB after testing. Parameter: &mb=4, default 3MB
+iconfast, iconmid, and iconslow respectively correspond to the icons when the speed is measured at fast, medium and slow speeds.
+colorlow, colormid, and colorhigh respectively correspond to the icon colors when the delay is low, medium, and high.
+Configuration example: title=Frills are productivity&iconfast=bird&iconmid=hare&iconslow=tortoise&colorlow=#06D6A0&colormid=#FFD166&colorhigh=#EF476F
 
-⚠️不想变化多端？？
-可直接使用最基本的panel参数，title、icon、icon-color
-配置实例：titile=不想花里胡哨了&icon=hare&icon-color=#CDCDCD
+⚠️Don’t want variety? ?
+You can directly use the most basic panel parameters, title, icon, icon-color
+Configuration example: titile=don’t want to be fancy&icon=hare&icon-color=#CDCDCD
 */
 const $ = new Env("network-speed");
 $.isPanel = () =>
@@ -32,7 +31,7 @@ let title = "";
 let content = "";
 !(async () => {
     if ($.isTile()) {
-        await notify("网络速率", "面板", "开始查询");
+        await notify("Network speed", "panel", "Start query");
     }
     const mb = $.lodash_get(arg, "mb") || 1;
     const bytes = mb * 1024 * 1024;
@@ -65,19 +64,19 @@ let content = "";
     console.log(`icon=shifts[${a}]:` + shifts[a]);
     console.log(`icon-color[${b}]:` + shifts[b]);
     title = `NetSpeed`;
-    content = `下行速率: ${round(Math.abs(speed * 8))} Mbps [${round(
+    content = `Download speed: ${round(Math.abs(speed * 8))} Mbps [${round(
         Math.abs(speed, 2),
         1
-    )} MB/s]\n测试耗时: ${round(
+    )} MB/s]\nTesting time: ${round(
         Math.abs(duration, 2),
         2
-    )}s\n网络延迟: ${pingt} ms\n执行时间: ${
+    )}s\nNetwork delay: ${pingt} ms\nExecution time: ${
         new Date().toTimeString().split(" ")[0]
     }`;
     if ($.isTile()) {
-        await notify("网络速率", "面板", "查询完成");
+        await notify("Network speed", "panel", "Query completed");
     } else if (!$.isPanel()) {
-        await notify("网络速率", title, content);
+        await notify("Network speed", title, content);
     }
 })()
     .catch(async (e) => {
@@ -88,7 +87,7 @@ let content = "";
         }`;
         title = `❌`;
         content = msg;
-        await notify("网络速率", title, content);
+        await notify("Network speed", title, content);
         $.log(title);
         $.log(content);
         const result = { title, content };

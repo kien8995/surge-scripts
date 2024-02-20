0b7982d02b89e992df21baadb9f8c04ac28d1ddf
kien8995 <trandongkien.sine@gmail.com>
2024-02-20 16:47:50 +0700

    update

--- sub-store/vmess-tcp-obfs-host.js
+++ sub-store/vmess-tcp-obfs-host.js
@@ -2,15 +2,16 @@
  * Modify obfuscated host for VMess 443 node
  * #host=google.com
  */
-function operator(proxies) {
+function operator(proxies, targetPlatform) {
     const { isSurge } = $substore.env;
     const { host, allowInsecure } = $arguments;
     if (isSurge) {
-        proxies.forEach((p) => {
-            if (p.type === "vmess" && p.port == 443) {
+        return proxies.map((proxy) => {
+            if (proxy.type === "vmess" && proxy.port === 443) {
                 proxy.sni = host;
-                proxy["skip-cert-verify"] = allowInsecure;
+                proxy["skip-cert-verify"] = allowInsecure == "true";
             }
+            return proxy;
         });
     }
 

a1c8eed9387b3b9fc07fef06311d543c284d0a88
kien8995 <trandongkien.sine@gmail.com>
2024-02-20 15:39:47 +0700

    update

--- sub-store/vmess-tcp-obfs-host.js
+++ sub-store/vmess-tcp-obfs-host.js
@@ -4,11 +4,15 @@
  */
 function operator(proxies) {
     const { isSurge } = $substore.env;
-    const { host } = $arguments;
-    proxies.forEach((p) => {
-        if (p.type === "vmess" && p.port == 443) {
-            proxy.sni = host;
-        }
-    });
+    const { host, allowInsecure } = $arguments;
+    if (isSurge) {
+        proxies.forEach((p) => {
+            if (p.type === "vmess" && p.port == 443) {
+                proxy.sni = host;
+                proxy["skip-cert-verify"] = allowInsecure;
+            }
+        });
+    }
+
     return proxies;
 }

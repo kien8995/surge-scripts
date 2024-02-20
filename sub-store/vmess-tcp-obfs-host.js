44bbea61dcee74cee843825ed617c5d573ac3a32
kien8995 <trandongkien.sine@gmail.com>
2024-02-20 15:35:14 +0700

    update

---
+++ sub-store/vmess-tcp-obfs-host.js
@@ -0,0 +1,14 @@
+/**
+ * Modify obfuscated host for VMess 443 node
+ * #host=google.com
+ */
+function operator(proxies) {
+    const { isSurge } = $substore.env;
+    const { host } = $arguments;
+    proxies.forEach((p) => {
+        if (p.type === "vmess" && p.port === 443) {
+            proxy.sni = host;
+        }
+    });
+    return proxies;
+}

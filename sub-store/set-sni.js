bb8279c0c7e5ecee4d1605065bab372c0f0c93b1
kien8995 <trandongkien.sine@gmail.com>
2024-03-09 06:12:50 +0700

    update

--- sub-store/set-sni.js
+++ sub-store/set-sni.js
@@ -1,17 +1,14 @@
 function operator(proxies) {
-    const { sni, allowInsecure, regex } = $arguments;
+    const { sni, allowInsecure } = $arguments;
     proxies.forEach((p) => {
-        if (!regex || (regex && decodeURIComponent(regex).test(p.name))) {
-            if (p.type === "vmess" && p.network === "ws") {
-                p["ws-opts"] = p["ws-opts"] || {};
-                p["ws-opts"]["headers"] = p["ws-opts"]["headers"] || {};
-                p["ws-opts"]["headers"]["Host"] = sni;
-            }
-            p.sni = sni;
-            if (!isRealValue(p["skip-cert-verify"])) {
-                p["skip-cert-verify"] =
-                    !allowInsecure || allowInsecure == "true";
-            }
+        if (p.type === "vmess" && p.network === "ws") {
+            p["ws-opts"] = p["ws-opts"] || {};
+            p["ws-opts"]["headers"] = p["ws-opts"]["headers"] || {};
+            p["ws-opts"]["headers"]["Host"] = sni;
+        }
+        p.sni = sni;
+        if (!isRealValue(p["skip-cert-verify"])) {
+            p["skip-cert-verify"] = !allowInsecure || allowInsecure == "true";
         }
     });
     return proxies;

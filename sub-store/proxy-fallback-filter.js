4945a5e081727dd7ad9790147443024dc09ce711
kien8995 <trandongkien.sine@gmail.com>
2024-03-22 21:22:06 +0700

    update

---
+++ sub-store/proxy-fallback-filter.js
@@ -0,0 +1,49 @@
+const SUB_NAME = "-master";
+const FALLBACK_SUB_NAME = "-slave";
+
+function operator(proxies = [], targetPlatform, context) {
+    let finalProxies = [];
+    finalProxies.push(
+        ...proxies.filter(
+            (p) =>
+                !p.subName.endsWith(SUB_NAME) &&
+                !p.subName.endsWith(FALLBACK_SUB_NAME)
+        )
+    );
+
+    const masterSlaveProxies = proxies.filter(
+        (p) =>
+            p.subName.endsWith(SUB_NAME) ||
+            p.subName.endsWith(FALLBACK_SUB_NAME)
+    );
+
+    const masterSlaveObject = groupProxy(masterSlaveProxies);
+    const keys = Object.keys(masterSlaveObject);
+    for (let key in keys) {
+        const keyProxies = masterSlaveObject[key];
+
+        if (keyProxies.some((p) => p.subName.endsWith(SUB_NAME))) {
+            finalProxies.push(
+                ...keyProxies.filter((p) => p.subName.endsWith(SUB_NAME))
+            );
+        } else {
+            finalProxies.push(
+                ...keyProxies.filter((p) =>
+                    p.subName.endsWith(FALLBACK_SUB_NAME)
+                )
+            );
+        }
+    }
+
+    return finalProxies;
+}
+
+function groupProxy(arr) {
+    return arr.reduce((store, proxy) => {
+        const key = proxy.subName.substring(0, proxy.subName.lastIndexOf("-"));
+        const keyStore = store[key] || (store[key] = []);
+        keyStore.push(proxy);
+
+        return store;
+    }, {});
+}
